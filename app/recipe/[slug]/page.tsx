461e7f2cf60dae4a75d0c79cfe03c68c8481b088
Adrian Humphreys <adrhumphreys@gmail.com>
2024-01-13 17:54:34 +1300

    Upgrades and static pages

--- app/recipe/[slug]/page.tsx
+++ app/recipe/[slug]/page.tsx
@@ -1,9 +1,17 @@
 import { Recipe } from "@/components/Recipe";
-import { getBySlug } from "@/lib/recipes";
+import { getBySlug, getRecipes } from "@/lib/recipes";
 
 type Props = {
   params: { slug: string };
 };
+
+export async function generateStaticParams() {
+  const recipes = await getRecipes();
+
+  return recipes.map(({ slug }) => ({
+    slug,
+  }));
+}
 
 export default async function RecipePage({ params: { slug } }: Props) {
   const recipe = await getBySlug(slug);
